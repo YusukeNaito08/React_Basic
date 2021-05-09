6187ce8b02b9505e8b13f5c2d51169592621b4bd
Yusuke Naito <75869555+YusukeNaito08@users.noreply.github.com>
2021-05-09 11:33:31 +0900

    Learn: (userState)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 //React17Update JSXのみ使用の場合　import必須では無い
 import ColorfullMessage from "./components/ColorfulMessage";
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink">元気です</ColorfullMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };

c9acd3f79135e89999177b48f7cdf784e01886fe
Yusuke Naito <75869555+YusukeNaito08@users.noreply.github.com>
2021-05-09 12:42:20 +0900

    Learn:UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 //React17Update JSXのみ使用の場合　import必須では無い
 import ColorfullMessage from "./components/ColorfulMessage";
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+        /* || は左側がfalseなら右側を返す */
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+        /* && は左側がtrueなら右側を返す */
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink">元気です</ColorfullMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(*^▽^*)</p>}
     </>
   );
 };
